eb471fd7d4409a489234e2a7860dbd372251c146
Kunja <kunjal.m@exhibit.co.in>
2025-07-04 14:49:35 +0530

    token

--- resources/views/layouts/apphome.blade.php
+++ resources/views/layouts/apphome.blade.php
@@ -7,6 +7,7 @@
     <title>{{ $seodata['MetaTitle']}}</title>
     <meta name="description" content="{{ $seodata['MetaDescription']}}">
     <meta name="keywords" content="{{ $seodata['Keyword']}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet">
