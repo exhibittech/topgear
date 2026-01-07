dc3d968c92a9a19b30d3d37efaea24852b33d55e
deependra04 <dvip34456@gmail.com>
2026-01-07 16:08:55 +0530

    cars fix

--- resources/views/awards/voting.blade.php
+++ resources/views/awards/voting.blade.php
@@ -1154,8 +1154,7 @@
             const categories = [
                 'cat1', 'cat2', 'cat3', 'cat4', 'cat5', 
                 'cat6', 'cat7', 'cat8', 'cat9', 'cat10', 
-                'cat11', 'cat12', 'cat13', 'cat14', 'cat15', 
-                'cat16', 'cat17', 'cat18', 'cat19'
+                'cat11', 'cat12', 'cat13', 'cat14', 'cat15'
             ];
     
             categories.forEach(function (category) {
