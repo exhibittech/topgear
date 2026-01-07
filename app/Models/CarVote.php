6b4c90ebe65187bc92caf5b7132efa5a2ec99f86
Sabir <Sabir.s@exhibit.co.in>
2026-01-07 15:32:45 +0530

    10 bike catagory and 1 car catagory added

--- app/Models/CarVote.php
+++ app/Models/CarVote.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class CarVote extends Model
 {
     use HasFactory;
-    protected $fillable = ['voting_user_id', 'cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6', 'cat7', 'cat8', 'cat9', 'cat10', 'cat11', 'cat12', 'cat13', 'cat14', 'cat15', 'cat16', 'cat17', 'cat18', 'cat19'];
+    protected $fillable = ['voting_user_id', 'cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6', 'cat7', 'cat8', 'cat9', 'cat10', 'cat11', 'cat12', 'cat13', 'cat14', 'cat15', 'cat16', 'cat17', 'cat18', 'cat19', 'cat20'];
 }
